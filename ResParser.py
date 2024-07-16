96ca7eee60888202e8d0ba50d46981240dac2507
MichaelJenkins <maj4k@yahoo.com>
2024-07-15 21:28:37 -0400

    structural changes

--- ResParser.py
+++ ResParser.py
@@ -1,25 +1,3 @@
-from flask import Flask, request, jsonify
-import json
+from pyresparser import ResumeParser
 
-app = Flask(__name__)
-users = [
-	{
-		'John':'password',
-		'activities' : [],
-	},
-	{
-		'Howard':'12345',
-		'activities' : [],
-	}
-]
-activities = {}
-
-
-@app.route("/",methods=["POST"])
-def create_user():
-	print(request.__dict__)
-	data = request.get_data()
-	return str(data),201
-
-if __name__ == "__main__":
-	app.run(debug=True)
+print("Hello")
